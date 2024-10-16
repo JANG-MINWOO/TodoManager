3cbdc7ac09d341d90f348d4bc7e422abe3868672
Minwoo <alsdn2606@gmail.com>
2024-10-16 16:34:30 +0900

    refactor: 사용되지 않은 로직 제거
    - 프로젝트 내에서 더 이상 사용되지 않는 불필요한 로직 및 코드 제거

--- src/main/java/com/sparta/practice/domain/member/repository/MemberRepository.java
+++ src/main/java/com/sparta/practice/domain/member/repository/MemberRepository.java
@@ -11,17 +11,4 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
-
-
-
-//    public Member findById(Long memberId) {
-//        String sql = "SELECT * FROM member WHERE id = ?";
-//        return jdbcTemplate.query(sql,rs ->{
-//            if(rs.next()){
-//                return Member.from(rs);
-//            } else{
-//                return null;
-//            }
-//        },memberId);
-//    }
 }
